694a07729c35bd8e3f26e9aee4fb7951a432f509
BugHunter-X <askzenai@gmail.com>
2025-01-30 12:36:05 +0100

    Add tailwind.config.js file

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+```javascript
+// Add the following to your index.html <head>
+<script>
+  tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          'custom-red': '#ff0000',
+        }
+      }
+    }
+  }
+</script>
+<script src="https://cdn.tailwindcss.com"></script>
+```
